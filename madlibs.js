1a4a921707d0df5d615410c3285cecda5065f177
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-08-23 22:53:08 +0300

    object created and appended

--- madlibs.js
+++ madlibs.js
@@ -27,12 +27,93 @@
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
 
+const REGEX = /(?<word>\w+)(?<pos>\[[nva]\])?(?<punctuation>[\.,])?/;
 
+const POS_FULL_WORD = {
+  n : "noun",
+  v : "verb",
+  a : "adjective",
+}
+
+function posFunction(param){
+  let nRegex = /[[n]]/;
+  let vRegex = /[[v]]/;
+  let aRegex = /[[a]]/;
+  if (nRegex.test(param) === true){
+    return nRegex = POS_FULL_WORD["n"]
+  } else if (vRegex.test(param) == true) {
+    return vRegex = POS_FULL_WORD.v
+  } else if (aRegex.test(param)){
+    return aRegex = POS_FULL_WORD.a
+  }
+}
 
 function parseStory(rawStory) {
   // Your code here.
-  return {}; // This line is currently wrong :)
+  const rawWords = rawStory.split(" ")
+  const results = [];
+
+  for (let i = 0; i < rawWords.length; i++){
+    const rawWord = rawWords[i];
+    const groups = REGEX.exec(rawWord).groups
+
+    results.push(
+      {
+        word: groups.word,
+        pos: groups.pos ? posFunction(groups.pos) : undefined,
+        punc: groups.punctuation,
+      }
+    )
+  }
+
+  const madlibsPrev = document.querySelector('.madLibsEdit');
+  for (let x = 0; x < results.length; x++){
+    let getWord = results[x].word
+    // console.log(getWord)
+    if (results[x].punc !== undefined){
+      madlibsPrev.innerHTML = madlibsPrev.innerHTML + results[x].punc
+    }
+    if (results[x].pos !== undefined){
+    results.pos = document.createElement('input')
+    const input = madlibsPrev.innerHTML + " " + '<input type="text" maxlength = "20" placeholder = "'+ results[x].pos + '" style= "border-radius:10px; width:15%; background-color:rgb(84, 240, 181); margin:3px" >'
+    madlibsPrev.innerHTML = input
+  } else  {
+    madlibsPrev.innerHTML = madlibsPrev.innerHTML + " " + getWord} 
+  }
+  
+  const madlibsOut = document.querySelector('.madLibsPreview');
+  const outSpan = document.createElement('span');
+
+  for (let y = 0; y < results.length; y++){
+    let getWord2 = results[y].word
+    // console.log(getWord2)
+    if (results[y].punc !== undefined){
+      madlibsOut.innerHTML = madlibsOut.innerHTML + results[y].punc
+    }
+    if (results[y].pos !== undefined){
+    results.pos = document.createElement('input')
+    const output = madlibsOut.innerHTML + " " + '<input type="text"  placeholder = "'+ results[y].pos + '" style= "border-radius:10px; width:15%; background-color:rgb(84, 240, 181); margin:3px" >'
+    madlibsOut.innerHTML = output
+  } 
+  else  {
+    madlibsOut.innerHTML = madlibsOut.innerHTML + " " + getWord2} 
+  }
+
+//   madlibsPrev.innerHTML.valueOf() = madlibsOut.innerHTML.valueOf();
+// // Add event listener on keyup
+// madlibsPrev.addEventListener('keyup', (event) => {
+  
+// })
+//   const name = event.key;
+//   const  code = event.code;
+//   // Alert the key name and key code on keydown
+ 
+// }, false);
+
+
+  return results; 
 }
+
 
 /**
  * All your other JavaScript code goes here, inside the function. Don't worry about

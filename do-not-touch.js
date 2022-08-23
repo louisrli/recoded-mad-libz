f0086c495d329d4c7ee790c8815dcac6f5694e6e
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2022-08-23 21:58:29 +0300

    paywand & marwa

--- do-not-touch.js
+++ do-not-touch.js
@@ -1,9 +1,8 @@
 /**
  * DO NOT TOUCH ANY OF THE CODE BELOW HERE.
- * 
+ *
  * Or you will be very sad.
  */
 const getRawStory = () => {
-  return fetch('./story.txt')
-    .then(response => response.text());
-};
+    return fetch('./story.txt').then((response) => response.text())
+}

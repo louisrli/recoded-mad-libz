f34e1d435d5ef0bc223a2208c3364fcea2f98d3e
Aland Abdullah <32212842+alandio@users.noreply.github.com>
2022-10-20 15:03:59 +0300

    Fetch the document from github instead of locally

--- do-not-touch.js
+++ do-not-touch.js
@@ -4,6 +4,6 @@
  * Or you will be very sad.
  */
 const getRawStory = () => {
-  return fetch('./story.txt')
+  return fetch('https://github.com/alandio/recoded-mad-libz/raw/master/story.txt')
     .then(response => response.text());
 };
